6a12ec8b530520f325139510f1db18ecdcab7a56
Gergely Czuczy <gergely.czuczy@harmless.hu>
2022-03-31 09:18:15 +0000

    Added ArrayProperty template

--- src/FGConveyorLift.cc
+++ src/FGConveyorLift.cc
@@ -3,11 +3,13 @@
 #include "GenericStruct.hh"
 #include "ArrayProperty.hh"
 #include "ObjectProperty.hh"
+#include "BoolProperty.hh"
 
 FGConveyorLift::FGConveyorLift(FGEntityType _et, Reader& _reader, FGObjectHeader& _fgoh, int32_t _throughput)
   : FGConveyorBelt(_et, _reader, _fgoh, _throughput) {
   defineProperty(std::make_shared<GenericStruct>("Transform", "mTopTransform"));
   defineProperty(std::make_shared<ArrayProperty<ObjectProperty> >("mSnappedPassthroughs", c_mSnappedPassthroughs));
+  defineProperty(std::make_shared<BoolProperty>("mIsReversed", c_mIsReversed));
 }
 
 FGConveyorLift::~FGConveyorLift() {
--- src/FGConveyorLift.hh
+++ src/FGConveyorLift.hh
@@ -13,8 +13,11 @@ public:
   FGConveyorLift(FGEntityType _et, Reader& _reader, FGObjectHeader& _fgoh, int32_t _throughput);
   virtual ~FGConveyorLift();
 
+  inline bool& mIsReversed() {return c_mIsReversed;};
+
 private:
   std::vector<ObjectReference> c_mSnappedPassthroughs;
+  bool c_mIsReversed;
   //virtual void deserialize(Reader &_reader);
 };
 
